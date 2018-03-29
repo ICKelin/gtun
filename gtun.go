e063bdca44455126503571941411445df465f8c7
yingjiu <yingjiu.zou@oceanblue.com>
2018-03-29 17:11:04 +0800

    add dhcp for client

--- gtun.go
+++ gtun.go
@@ -3,8 +3,10 @@ package main
 import (
 	"encoding/binary"
 	"flag"
+	"fmt"
 	"net"
 	"os"
+	"os/exec"
 	"os/signal"
 	"syscall"
 
@@ -37,20 +39,24 @@ func main() {
 		return
 	}
 
+	tunip, err := GetTunIP(conn)
+	if err != nil {
+		glog.ERROR(err)
+		return
+	}
+
+	err = SetTunIP(*pdev, tunip)
+	if err != nil {
+		glog.ERROR(err)
+		return
+	}
+
 	go IfaceRead(ifce, conn)
 	go IfaceWrite(ifce, conn)
 
 	sig := make(chan os.Signal, 3)
 	signal.Notify(sig, syscall.SIGINT, syscall.SIGABRT, syscall.SIGHUP)
 	<-sig
-}
-
-func ConServer(srv string) (conn net.Conn, err error) {
-	conn, err = net.Dial("tcp", srv)
-	if err != nil {
-		return nil, err
-	}
-	return conn, err
 }
 
 func IfaceRead(ifce *water.Interface, conn net.Conn) {
@@ -104,4 +110,51 @@ func ForwardSrv(srvcon net.Conn, buff []byte) (err error) {
 	}
 
 	return err
+}
+
+func ConServer(srv string) (conn net.Conn, err error) {
+	conn, err = net.Dial("tcp", srv)
+	if err != nil {
+		return nil, err
+	}
+	return conn, err
+}
+
+func GetTunIP(conn net.Conn) (tunip string, err error) {
+	plen := make([]byte, 4)
+	nr, err := conn.Read(plen)
+	if err != nil {
+		return "", err
+	}
+
+	if nr != 4 {
+		return "", fmt.Errorf("too short pkt")
+	}
+
+	payloadlength := binary.BigEndian.Uint32(plen)
+	buff := make([]byte, int(payloadlength))
+
+	nr, err = conn.Read(buff)
+	if err != nil {
+		return "", err
+	}
+
+	return string(buff[:nr]), nil
+}
+
+func SetTunIP(dev, tunip string) (err error) {
+	uptun := fmt.Sprintf("ifconfig %s up", dev)
+	setip := fmt.Sprintf("ip addr add %s/24 dev %s", tunip, dev)
+
+	err = exec.Command("/bin/sh", "-c", uptun).Run()
+	if err != nil {
+		return fmt.Errorf("up %s error %s", dev, err.Error())
+	}
+
+	err = exec.Command("/bin/sh", "-c", setip).Run()
+	if err != nil {
+		return fmt.Errorf("up %s error %s", dev, err.Error())
+	}
+
+	return nil
 }
--- gtun_srv.go
+++ gtun_srv.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"encoding/binary"
+	"fmt"
 	"io"
 	"net"
 
 	"github.com/ICKelin/glog"
+	"github.com/songgao/water"
 )
 
-var client = make([]net.Conn, 0)
+var client = make(map[string]net.Conn)
+var ippool = make(map[string]bool)
+
+func init() {
+	for i := 10; i < 250; i++ {
+		ip := fmt.Sprintf("10.10.253.%d", i)
+		ippool[ip] = false
+	}
+}
 
 func main() {
 	listener, err := net.Listen("tcp", ":9621")
@@ -15,6 +26,18 @@ func main() {
 		glog.ERROR(err)
 		return
 	}
+
+	cfg := water.Config{
+		DeviceType: water.TUN,
+	}
+	cfg.Name = "gtun_srv"
+	ifce, err := water.New(cfg)
+
+	if err != nil {
+		glog.ERROR(err)
+		return
+	}
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -22,14 +45,29 @@ func main() {
 			break
 		}
 
-		client = append(client, conn)
 		glog.INFO("accept gtun client")
-		go HandleClient(conn)
+		go HandleClient(conn, ifce)
 	}
 }
 
-func HandleClient(conn net.Conn) {
+func HandleClient(conn net.Conn, ifce *water.Interface) {
 	defer conn.Close()
+
+	cip, err := SelectIP()
+	if err != nil {
+		glog.ERROR(err)
+		return
+	}
+
+	defer RecycleIP(cip)
+
+	client[cip] = conn
+	defer delete(client, cip)
+
+	if err := DHCP(conn, cip); err != nil {
+		glog.ERROR("dhcp ip for client fail", err)
+		return
+	}
 
 	buff := make([]byte, 65536)
 	for {
@@ -41,12 +79,47 @@ func HandleClient(conn net.Conn) {
 			break
 		}
 
-		// broadcast
-		// TODO select peer
-		for _, c := range client {
-			if c.RemoteAddr().String() != conn.RemoteAddr().String() {
-				c.Write(buff[:nr])
+		if nr < 25 {
+			glog.ERROR("too short ippkt")
+			continue
+		}
+
+		dst := fmt.Sprintf("%d.%d.%d.%d", buff[20], buff[21], buff[22], buff[23])
+
+		c := client[dst]
+		if c != nil {
+			c.Write(buff[:nr])
+		} else {
+			_, err = ifce.Write(buff[4:nr])
+			if err != nil {
+				glog.ERROR(err)
 			}
 		}
 	}
+}
+
+func DHCP(conn net.Conn, clientip string) (err error) {
+	plen := make([]byte, 4)
+	binary.BigEndian.PutUint32(plen, uint32(len(clientip)))
+
+	payload := make([]byte, 0)
+	payload = append(payload, plen...)
+	payload = append(payload, []byte(clientip)...)
+
+	_, err = conn.Write(payload)
+	return err
+}
+
+func SelectIP() (ip string, err error) {
+	for ip, v := range ippool {
+		if v == false {
+			ippool[ip] = true
+			return ip, nil
+		}
+	}
+	return "", fmt.Errorf("not enough ip in pool")
+}
+
+func RecycleIP(cip string) {
+	ippool[cip] = false
 }

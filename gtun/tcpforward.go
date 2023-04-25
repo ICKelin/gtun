60a664ab3df62ae60a2bf11dd6594defa78d3492
ICKelin <yingjiu.hulu@gmail.com>
2023-04-25 20:40:28 +0800

    feature: add tcpforward ratelimit

--- gtun/tcpforward.go
+++ gtun/tcpforward.go
@@ -168,11 +168,43 @@ func (f *TCPForward) forwardDirect(conn net.Conn, dip, dport string) {
 		obj := f.mempool.Get()
 		defer f.mempool.Put(obj)
 		buf := obj.([]byte)
-		io.CopyBuffer(rconn, conn, buf)
+		copyBuffer(rconn, conn, buf, f.ratelimit)
 	}()
 
 	obj := f.mempool.Get()
 	defer f.mempool.Put(obj)
 	buf := obj.([]byte)
-	io.CopyBuffer(conn, rconn, buf)
+	copyBuffer(conn, rconn, buf, f.ratelimit)
+}
+
+func copyBuffer(dst io.Writer, src io.Reader, buf []byte, limiter *RateLimit) (written int64, err error) {
+	for {
+		nr, er := src.Read(buf)
+		if nr > 0 {
+			limiter.LimitRate(int64((nr)))
+			nw, ew := dst.Write(buf[0:nr])
+			if nw < 0 || nr < nw {
+				nw = 0
+				if ew == nil {
+					ew = fmt.Errorf("invalid write")
+				}
+			}
+			written += int64(nw)
+			if ew != nil {
+				err = ew
+				break
+			}
+			if nr != nw {
+				err = io.ErrShortWrite
+				break
+			}
+		}
+		if er != nil {
+			if er != io.EOF {
+				err = er
+			}
+			break
+		}
+	}
+	return written, err
 }
